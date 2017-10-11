488b3dde507f7714d4cbc47c0697cd5bc3edfe6c
bwain <bryandwain@gmail.com>
2017-10-11 05:50:00 -0500

    fix exports for mocking

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import c from './combine-reducers';
 import m from './merge-child-reducers';
 
-export const combineReducers = c;
-export const mergeChildReducers = m;
+//by exporting functions, these are able to be spied on
+export function combineReducers(...args){return c(...args);}
+export function mergeChildReducers(...args){return m(...args);}
